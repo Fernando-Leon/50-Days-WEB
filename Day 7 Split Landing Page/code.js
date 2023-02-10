431f3cc37ec219abcc13c61cc982f88c84a5ba1e
Fernando-Leon <96993869+Fernando-Leon@users.noreply.github.com>
2023-02-09 18:09:42 -0600

    Complete Day 7 Full

--- Day 7 Split Landing Page/code.js
+++ Day 7 Split Landing Page/code.js
@@ -2,18 +2,8 @@ const main = document.getElementById('main');
 const play = document.getElementById('play');
 const xbox = document.getElementById('xbox');
 
-play.addEventListener("mousemove", () =>{
-    main.style.gridTemplateColumns = '5fr 1fr';
-});
+play.addEventListener("mousemove", () =>{ main.style.gridTemplateColumns = '4fr 1fr'; });
+play.addEventListener("mouseleave", () =>{ main.style.gridTemplateColumns = '1fr 1fr'; });
 
-play.addEventListener("mouseleave", () =>{
-    main.style.gridTemplateColumns = '1fr 1fr';
-});
-
-xbox.addEventListener("mousemove", () =>{
-    main.style.gridTemplateColumns = '5fr 1fr';
-});
-
-xbox.addEventListener("mouseleave", () =>{
-    main.style.gridTemplateColumns = '1fr 1fr';
-});
+xbox.addEventListener("mousemove", () =>{ main.style.gridTemplateColumns = '1fr 4fr' });
+xbox.addEventListener("mouseleave", () =>{ main.style.gridTemplateColumns = '1fr 1fr'; });
